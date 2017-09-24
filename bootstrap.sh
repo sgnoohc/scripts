f366fb4d0ed4ef8d4b5aba1838c313d228412212
Philip Chang <sgnoohc@gmail.com>
2017-09-23 23:21:54 -0700

    some typos and adding setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,6 @@ git clone git@github.com:cmstas/CORE.git
 git clone git@github.com:aminnj/ProjectMetis.git
 git clone git@github.com:sgnoohc/WWW_CORE.git
 git clone git@github.com:sgnoohc/rooutil.git
-git clone git@github.com:sgnoohc/scanchains.gi
+git clone git@github.com:sgnoohc/scanchains.git
+git clone git@github.com:sgnoohc/scripts.git
+source scripts/setup.sh
