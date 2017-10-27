aa4ade7730e1d01ba3a17ea0f685cd8e29a7f9eb
Philip Chang <sgnoohc@gmail.com>
2017-10-27 12:07:40 -0700

    update'

--- printsf.py
+++ printsf.py
@@ -18,8 +18,8 @@ def printsf(funcname, xthreshs, ythreshs, sfs, errs, filename="", xvar="eta", yv
     funcstr = ""
     funcstr += "float {}(float {}, float {}, int isyst=0)\n".format(funcname, yvar, xvar)
     funcstr += "{\n"
-    funcstr += "    if (isyst != 1 && isyst != -1 && isyst != 0)"
-    funcstr += "        printf(Form(\"WARNING - in function=%s, isyst=%d is not recommended!\n\", __FUNCTION__, isyst));"
+    funcstr += "    if (isyst != 1 && isyst != -1 && isyst != 0)\n"
+    funcstr += "        printf(Form(\"WARNING - in function=%s, isyst=%d is not recommended!\\n\", __FUNCTION__, isyst));\n"
     for i, xthresh in enumerate(xthreshs):
         for j, ythresh in enumerate(ythreshs):
             sf = sfs[i][j]
@@ -53,36 +53,26 @@ def printsf_th2(funcname, th2, filename="", xvar="eta", yvar="pt", xvarabs=False
             errs[i-1].append(th2.GetBinError(i, j))
 
     printsf(funcname, xthreshs, ythreshs, sfs, errs, filename, xvar, yvar, xvarabs, yvarabs)
-#
-#    # parse some options and process some stuff
-#    yvarabsstr = ""
-#    xvarabsstr = ""
-#    if yvarabs: yvarabsstr = "fabs"
-#    if xvarabs: xvarabsstr = "fabs"
-#
-#    # Form the function sring
-#    funcstr = ""
-#    funcstr += "float {}(float {}, float {}, int isyst=0)\n".format(funcname, yvar, xvar)
-#    funcstr += "{\n"
-#    funcstr += "    if (isyst != 1 && isyst != -1 && isyst != 0)"
-#    funcstr += "        printf(Form(\"WARNING - in function=%s, isyst=%d is not recommended!\n\", __FUNCTION__, isyst));"
-#    for i in xrange(1, th2.GetNbinsX() + 1):
-#        for j in xrange(1, th2.GetNbinsY() + 1):
-#            sf = th2.GetBinContent(i, j)
-#            err = th2.GetBinError(i, j)
-#            xthresh = th2.GetXaxis().GetBinUpEdge(i)
-#            ythresh = th2.GetYaxis().GetBinUpEdge(j)
-#            funcstr += "    if ({}({}) < {} && {}({}) < {}) return {} + isyst * {};\n".format(yvarabsstr, yvar, ythresh, xvarabsstr, xvar, xthresh, sf, err)
-#    funcstr += "    return 1;\n"
-#    funcstr += "}\n"
-#
-#    # print or write to file
-#    if len(filename) != 0:
-#        f = open(filename, "w")
-#        f.write(funcstr)
-#    else:
-#        print funcstr
 
-#def printsf_tgraph(funcname, tgraph, filename="", 
+def printsf_tgraph1d(funcname, tgraph1d, filename="", xvar="eta", yvar="pt", xvarabs=False, yvarabs=False):
+    """
+    Function to print scale factors (or fake rate) from 1D TGraph
+    WARNING: this only takes one side of the error
+    """
+    npoints = tgraph1d.GetN()
+    xs = tgraph1d.GetX()
+    ys = tgraph1d.GetY()
+    xerrs = tgraph1d.GetEX()
+    yerrs = tgraph1d.GetEY()
+    xthreshs = [1000000]
+    ythreshs = []
+    sfs = [[]]
+    errs = [[]]
+    for index, x in enumerate(xs):
+        ythreshs.append(x + tgraph1d.GetErrorXhigh(index))
+    for index, y in enumerate(ys):
+        sfs[0].append(y)
+        errs[0].append(tgraph1d.GetErrorYhigh(index))
+    printsf(funcname, xthreshs, ythreshs, sfs, errs, filename, xvar, yvar, xvarabs, yvarabs)
 
 #eof
