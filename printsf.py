4c9402f375d1dc8def7e8aa2410434b7c9785928
Philip Chang <sgnoohc@gmail.com>
2017-10-27 10:59:34 -0700

    update

--- printsf.py
+++ printsf.py
@@ -5,14 +5,20 @@ import sys
 import ROOT as r
 
 
-def printsf(funcname, th2, filename=""):
+def printsf_th2(funcname, th2, filename="", xvar="eta", yvar="pt", xvarabs=False, yvarabs=False):
     """
     Function to print scale factors (or fake rate) from TH2
     """
 
+    # parse some options and process some stuff
+    yvarabsstr = ""
+    xvarabsstr = ""
+    if yvarabs: yvarabsstr = "fabs"
+    if xvarabs: xvarabsstr = "fabs"
+
     # Form the function sring
     funcstr = ""
-    funcstr += "float {}(float pt, float eta, int isyst=0)\n".format(funcname)
+    funcstr += "float {}(float {}, float {}, int isyst=0)\n".format(funcname, yvar, xvar)
     funcstr += "{\n"
     funcstr += "    if (isyst != 1 && isyst != -1 && isyst != 0)"
     funcstr += "        printf(Form(\"WARNING - in function=%s, isyst=%d is not recommended!\n\", __FUNCTION__, isyst));"
@@ -20,9 +26,9 @@ def printsf(funcname, th2, filename=""):
         for j in xrange(1, th2.GetNbinsY() + 1):
             sf = th2.GetBinContent(i, j)
             err = th2.GetBinError(i, j)
-            etathresh = th2.GetXaxis().GetBinUpEdge(i)
-            ptthresh = th2.GetYaxis().GetBinUpEdge(j)
-            funcstr += "    if (pt < {} && fabs(eta) < {}) return {} + isyst * {};\n".format(ptthresh, etathresh, sf, err)
+            xthresh = th2.GetXaxis().GetBinUpEdge(i)
+            ythresh = th2.GetYaxis().GetBinUpEdge(j)
+            funcstr += "    if ({}({}) < {} && {}({}) < {}) return {} + isyst * {};\n".format(yvarabsstr, yvar, ythresh, xvarabsstr, xvar, xthresh, sf, err)
     funcstr += "    return 1;\n"
     funcstr += "}\n"
 
